a387e2bf940d583f3a5752c7c304752aa789e071
bookmaker01 <booker4423@gmail.com>
2020-12-08 18:38:18 +0900

    商品編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show ]
+  before_action :contributor_confirmation, only: :edit
   def index
    @items = Item.order(id: :DESC).includes(:user)
 
@@ -22,9 +23,26 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+   @item = Item.find(params[:id])
+   if @item.update(item_params)
+    redirect_to item_path(@item)
+   else
+    render :edit
+   end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :image, :category_id, :price, :status_id, :burden_id, :area_id, :guideline_id, :explanation).merge(user_id: current_user.id)
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @item.user
   end
 end
