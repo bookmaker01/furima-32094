cead073160108ac43be7c65d416d9c78b91d2c91
bookmaker01 <booker4423@gmail.com>
2020-12-08 19:39:39 +0900

    修正二回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show ]
-  before_action :set_item, only: :edit
+  before_action :set_item, only: [:edit, :show, :update]
   before_action :contributor_confirmation, only: :edit
   def index
    @items = Item.order(id: :DESC).includes(:user)
@@ -21,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-   @item = Item.find(params[:id])
    if @item.update(item_params)
     redirect_to item_path(@item)
    else
