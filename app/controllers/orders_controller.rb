6708e7ab38a360a2d00883566502e0de6816774e
bookmaker01 <booker4423@gmail.com>
2020-12-16 13:12:59 +0900

    修正二回目

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,10 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+  before_action :contributor_confirmation, only: [:index]
 
 def index
-  @item = Item.find(params[:item_id])
+  
   @form =Form.new
 end
 
@@ -10,7 +13,6 @@ def new
 end
 
 def create
-  @item = Item.find(params[:item_id])
   @form = Form.new(order_params)   
    if @form.valid?
     Payjp.api_key = "sk_test_48d80d33fafada7a820920bf"  
@@ -27,7 +29,13 @@ def create
 end
 
 private
+def set_item
+  @item = Item.find(params[:item_id])
+end
 def order_params
   params.permit(:token,:postal_code,:prefectures_id,:municipality,:address,:building_name,:phone_number,:item_id).merge(user_id: current_user.id)
+end
+def contributor_confirmation
+  redirect_to root_path if current_user.id == @item.user_id || @item.order
 end
 end
